9849aab238a19c7e589b8870b56b651b1fe20f24
Kieu Trang <kieutrang1720@gmail.com>
2020-03-16 11:16:39 +0100

    aaa

---
+++ TestGitForm/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitForm
+{
+    class Class1
+    {
+        public Class1()
+        {
+
+        }
+        
+        public int Devide(int a, int b)
+        {
+            return a / b;
+        }
+		
+		 public int Devide1(int a, int b)
+        {
+            return a / b;
+        }
+		
+		 public int Devide2(int a, int b)
+        {
+            return a / b;
+        }
+    }
+
+    class Class12
+    {
+        public Class12()
+        {
+
+        }
+
+        public int Devide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
