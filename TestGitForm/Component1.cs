2232dfb7de6a44af4157b5bdd740b0536c174222
Kieu Trang <kieutrang1720@gmail.com>
2020-03-17 04:35:43 +0100

    commit local file

---
+++ TestGitForm/Component1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitForm
+{
+    public partial class Component1 : Component
+    {
+        public Component1()
+        {
+            InitializeComponent();
+        }
+
+        public Component1(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+
+            string a = "test commit local changes";
+            string a = "test commit local changes";
+            string a = "test commit local changes";
+        }
+    }
+}
